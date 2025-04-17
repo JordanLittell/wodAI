3f159e672a94cbfb24c11dee0c4879a8b6b4e9a3
Jordan Littell <jordanlittell@gmail.com>
2025-04-16 21:24:27 -0700

    Initial Commit

---
+++ wodAITests/wodAITests.swift
@@ -0,0 +1,17 @@
+//
+//  wodAITests.swift
+//  wodAITests
+//
+//  Created by Jordan Littell on 4/16/25.
+//
+
+import Testing
+@testable import wodAI
+
+struct wodAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
